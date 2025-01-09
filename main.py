aa3ed442633f8963c5a422ca0c82bba37b866f28
Anthony Popoca <tonystr97@gmail.com>
2025-01-09 15:47:10 -0600

    report still needs to be properly sorted

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def main():
     text = get_book_text(book_path)
     num_words = count_words(text)
     letter_dict = count_characters(text)
-    print(letter_dict)
+    sorted = letter_sorted(letter_dict)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    for c in sorted:
+        print(f"The {c} character was found {sorted[c]} times")
+    print("--- End report ---")
 
 
 
@@ -20,15 +25,22 @@ def count_characters(text):
     letter = {}
     for c in text:
         lowered = c.lower()
-        if lowered in letter:
-            letter[lowered] += 1
-        else:
-            letter[lowered] = 1
+        if lowered.isalpha():
+            if lowered in letter:
+                letter[lowered] += 1
+            else:
+                letter[lowered] = 1
     return letter
 
+def sort_on(letter_dict):
+    return letter_dict["num"]
 
-
-
+def letter_sorted(num_chars_dict):
+    sorted = []
+    for l in num_chars_dict:
+        sorted.append({"char": l, "num": num_chars_dict[l]})
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted
 
 
 
